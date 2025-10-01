3821344862f9ab30aea4083e55aa3037463f815d
Ahmed Abdallah <ahmedismail14141414@gmail.com>
2025-10-02 02:21:32 +0300

    test docker

--- ShoppingApp/Program.cs
+++ ShoppingApp/Program.cs
@@ -1,32 +1,39 @@
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using ShoppingApp.Data;
 using ShoppingApp.Repository;
 using ShoppingApp.Repository.Base;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Configure Kestrel to listen on port 80 inside the container
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(80); // مهم للاتصال من Docker
+});
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<AppDBContext>(options => options.UseSqlServer(
-    builder.Configuration.GetConnectionString("DBConnection")) );
-builder.Services.AddTransient(typeof(IRepository<>),typeof(MainRepository<>));
-builder.Services.AddTransient<IUnitOfWork,UnitOfWork>();
+builder.Services.AddDbContext<AppDBContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DBConnection")));
+builder.Services.AddTransient(typeof(IRepository<>), typeof(MainRepository<>));
+builder.Services.AddTransient<IUnitOfWork, UnitOfWork>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection(); // علقناها للـ Docker
 app.UseStaticFiles();
 
 app.UseRouting();
 
 app.UseAuthorization();
+
 app.MapControllerRoute(
     name: "area",
     pattern: "{area:exists}/{controller=Employee}/{action=Index}/{id?}");
